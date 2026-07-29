0c8d66f5e946e7c9266b400636bccc8a2045573c
Devendra Barman <97818502+Venom7x@users.noreply.github.com>
2026-07-29 20:37:12 +0530

    Update SecurityConfig.java

--- src/main/java/com/blogplatform/config/SecurityConfig.java
+++ src/main/java/com/blogplatform/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
         
         configuration.setAllowedOrigins(List.of(
             "http://localhost:5173",
-            "http://localhost:3000"
+            "http://localhost:3000",
+            "https://blog-platform-frontend-4ohqd97fu-null-void1.vercel.app/"
         ));
         
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
